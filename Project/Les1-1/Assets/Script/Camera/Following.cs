4afa98eb2ed4314bcecbcb770ab46cac1033a9d5
BaoQingwu <940734259@qq.com>
2019-06-11 18:00:06 +0800

    Les1-demo

---
+++ Project/Les1-1/Assets/Script/Camera/Following.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Following : MonoBehaviour
+{
+    public Transform target = null;
+
+    private Camera CurCamera = null;
+
+    public float fDistance = 10.0f;
+
+    public float fAngle = 45.0f;
+
+    private Vector3 ForWard = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurCamera = GetComponent<Camera>();
+        if(null != target && CurCamera != null)
+        {
+            CurCamera.transform.LookAt(target, Vector3.up);
+            ForWard = target.forward.normalized;
+            float value = Mathf.Cos(Mathf.Deg2Rad * fAngle);
+            
+        }
+       
+    }
+
+    private void LateUpdate()
+    {
+        UpdateCameraPosition(target, CurCamera.transform);
+    }
+
+    #region FUN
+    private void UpdateCameraPosition(Transform Target,Transform Cam)
+    {
+        
+    }
+    #endregion
+
+}
