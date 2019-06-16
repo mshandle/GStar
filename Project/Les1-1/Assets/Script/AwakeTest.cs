d2818e610acd9262238634daedf12d41c463b1b9
BaoQingwu <940734259@qq.com>
2019-06-16 20:05:09 +0800

    2

---
+++ Project/Les1-1/Assets/Script/AwakeTest.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class AwakeTest : MonoBehaviour
+{
+    static int AwakeCount = 0;
+    private int ID = 0;
+    private void Awake()
+    {
+        ID = AwakeCount;
+        Debug.LogFormat("Awake Index:{0}", AwakeCount++);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogFormat("Update ID：{0}", ID);
+    }
+}
