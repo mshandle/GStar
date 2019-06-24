2b144b6801076ce80e1e288f596ef403f7efff16
BaoQingwu <940734259@qq.com>
2019-06-24 15:23:33 +0800

    5

---
+++ Project/Les1-1/Assets/Script/AwakeWithNameEx.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeWithNameEx : MonoBehaviour
+{
+    [SerializeField]
+    public string name = string.Empty;
+
+    private void Awake()
+    {
+        Debug.LogFormat("Awake extension With:{0}", name);
+    }
+}
