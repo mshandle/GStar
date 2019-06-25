c3cec7a2328cbc50c9a5285a6c9f6f184c7a781e
BaoQingwu <940734259@qq.com>
2019-06-25 18:32:37 +0800

    6

---
+++ Project/Les2/Assets/Example/Example1/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test {
+
+    public class MouseEvent
+    {
+        public int touchX;
+        public int touchY;
+    }
+
+    public class Graphic
+    {
+        public void DrawLine(Point Star, Point End)
+        {
+
+        }
+
+        public void DrawRectangel(Point point,float width, float height)
+        {
+
+        }
+
+    }
+
+
+    public class MainForm : MonoBehaviour
+    {
+        private List<Line> lines = new List<Line>();
+
+        private List<Rect> rects = new List<Rect>();
+        //change
+        private bool isCheckLine = false;
+        private bool isCheckRect = false;
+
+        private Point LastTouch = new Point();
+        public void OnMouseDown(MouseEvent e)
+        {
+            LastTouch.x = e.touchX;
+            LastTouch.y = e.touchX;
+        }
+
+        public void OnMouseUp(MouseEvent e)
+        {
+            Point curTouch = new Point();
+            curTouch.x = e.touchX;
+            curTouch.y = e.touchY;
+            if (isCheckLine)
+            {
+                lines.Add(new Line(LastTouch, curTouch));
+            }
+
+            if (isCheckRect)
+            {
+                float width = Mathf.Abs(curTouch.x - LastTouch.x);
+                float height = Mathf.Abs(curTouch.y - LastTouch.y);
+                rects.Add(new Rect(LastTouch, width, height));
+            }
+        }
+
+        public void OnDraw(Graphic graphic)
+        {
+            //...
+            foreach (var e in lines)
+            {
+                graphic.DrawLine(e.Star, e.end);
+            }
+
+            foreach(var e in rects)
+            {
+                graphic.DrawRectangel(e.point, e.x, e.y);
+            }
+
+            //...
+        }
+    }
+
+}
+
+namespace Test2
+{
+
+    public class MouseEvent
+    {
+        public int touchX;
+        public int touchY;
+    }
+
+    public class Graphic
+    {
+        public void DrawLine(Point Star, Point End)
+        {
+
+        }
+
+        public void DrawRectangel(Point point, float width, float height)
+        {
+
+        }
+
+    }
+
+
+    public class MainForm : MonoBehaviour
+    {
+        private List<Sharp> sharps = new List<Sharp>();
+
+        private bool isCheckLine = false;
+        private bool isCheckRect = false;
+
+        private Point LastTouch = new Point();
+        public void OnMouseDown(MouseEvent e)
+        {
+            LastTouch.x = e.touchX;
+            LastTouch.y = e.touchX;
+        }
+
+        public void OnMouseUp(MouseEvent e)
+        {
+            Point curTouch = new Point();
+            curTouch.x = e.touchX;
+            curTouch.y = e.touchY;
+            if (isCheckLine)
+            {
+                sharps.Add(new Line(LastTouch, curTouch));
+            }
+
+            if (isCheckRect)
+            {
+                float width = Mathf.Abs(curTouch.x - LastTouch.x);
+                float height = Mathf.Abs(curTouch.y - LastTouch.y);
+                sharps.Add(new Rect(LastTouch, width, height));
+            }
+        }
+
+        public void OnDraw(Graphic graphic)
+        {
+            //...
+            foreach (var e in sharps)
+            {
+                e.Draw(graphic);
+            }
+            //...
+        }
+    }
+
+}
---
+++ Project/Les2/Assets/Example/Example1/Sharp.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Test
+{
+    public struct Point
+    {
+        public int x;
+        public int y;
+    }
+
+    public class Line
+    {
+        public Point Star;
+        public Point end;
+
+        public Line(Point x, Point y)
+        {
+            this.Star = x;
+            this.end = y;
+        }
+    }
+
+    public class Rect
+    {
+        public Point point;
+        public float x;
+        public float y;
+
+        public Rect(Point point, float x, float y)
+        {
+            this.point = point;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
+/// <summary>
+/// 
+/// </summary>
+namespace Test2
+{
+    public abstract class Sharp
+    {
+        public abstract void Draw(Graphic drawer);
+    }
+    public struct Point
+    {
+        public int x;
+        public int y;
+    }
+
+    public class Line : Sharp
+    {
+        public Point Star;
+        public Point end;
+
+        public Line(Point x, Point y)
+        {
+            this.Star = x;
+            this.end = y;
+        }
+        public override void Draw(Graphic drawer) 
+        {
+            drawer.DrawLine(Star, end);
+        }
+    }
+
+    public class Rect:Sharp
+    {
+        public Point point;
+        public float x;
+        public float y;
+
+        public Rect(Point point, float x, float y)
+        {
+            this.point = point;
+            this.x = x;
+            this.y = y;
+        }
+
+        public override void Draw(Graphic drawer)
+        {
+            drawer.DrawRectangel(point,x, y);
+        }
+    }
+}
+
